f5b7d5918fb7164811a36112af14166646502866
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2021-10-31 20:44:04 -0300

    :lipstick: create styles of the PlayerScore component

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -4,18 +4,21 @@ import PlayerGame from '../../components/PlayerGame'
 import PlayerPoints from '../../components/PlayerPoints'
 import VsPlayer from '../../components/VsPlayer'
 import ArrowPlayer from '../../components/ArrowPlayer'
+import PlayerScore from '../../components/PlayerScore'
 
-const $htmlArrowPlayer = ArrowPlayer()
+// const $htmlArrowPlayer = ArrowPlayer()
 const $htmlPlayerGame1 = PlayerGame('Player1')
 const $htmlPlayerGame2 = PlayerGame('Player2')
 const $htmlVsPlayer = VsPlayer('vs')
-const $htmlPlayerPoints1 = PlayerPoints()
-const $htmlPlayerPoints2 = PlayerPoints()
+const $htmlPlayerPoints1 = PlayerScore(0)
+const $htmlPlayerPoints2 = PlayerScore(3)
+
+// const $htmlPlayerPoints1 = PlayerPoints()
+// const $htmlPlayerPoints2 = PlayerPoints()
 
 const ScoreBoard = () => {
     return `      
-        <header class="score-board">
-            ${$htmlArrowPlayer}
+        <header class="score-board">        
             ${$htmlPlayerGame1}                     
             ${$htmlPlayerPoints1}
             ${$htmlVsPlayer}          
