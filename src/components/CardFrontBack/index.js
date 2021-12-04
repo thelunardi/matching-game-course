3cb25c7140be83a2041ce3450246080b3a206b41
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2021-12-04 09:00:24 -0300

    :bug: swap player when flip two cards

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -4,31 +4,33 @@ import CardGame from '../CardGame'
 const CardFrontBack = (icon, altIcon) => {
     window.cardFrontBack = {}
     window.cardFrontBack.handleClick = (event) => {
-        const cardFrontBack = document.getElementsByClassName('-active')
-        const $cardFrontBack = event.target.closest('.card-front-back')
+        const $origin = event.target;
+        // const cardFrontBack = document.getElementsByClassName('-active')
+        const $cardFrontBack = $origin.closest('.card-front-back')
+
         $cardFrontBack.classList.toggle('-active')
 
-        if (cardFrontBack.length === 2) {
-            setTimeout(() => {
-                while (cardFrontBack[0]) {
-                    cardFrontBack[0].classList.remove('-active')
-                }
+        // if (cardFrontBack.length === 2) {
+        //     setTimeout(() => {
+        //         while (cardFrontBack[0]) {
+        //             cardFrontBack[0].classList.remove('-active')
+        //         }
 
-                const playerOneArrow = document.getElementsByClassName('-player-one')
-                const playerTwoArrow = document.getElementsByClassName('-player-two')
+        //         const playerOneArrow = document.getElementsByClassName('-player-one')
+        //         const playerTwoArrow = document.getElementsByClassName('-player-two')
 
-                if (playerOneArrow.length > 0) {
-                    playerOneArrow[0].classList.toggle('-player-two')
-                    playerOneArrow[0].classList.toggle('-player-one')
-                    return
-                }
+        //         if (playerOneArrow.length > 0) {
+        //             playerOneArrow[0].classList.toggle('-player-two')
+        //             playerOneArrow[0].classList.toggle('-player-one')
+        //             return
+        //         }
 
-                if (playerTwoArrow.length > 0) {
-                    playerTwoArrow[0].classList.toggle('-player-one')
-                    playerTwoArrow[0].classList.toggle('-player-two')
-                }
-            }, 1000)
-        }
+        //         if (playerTwoArrow.length > 0) {
+        //             playerTwoArrow[0].classList.toggle('-player-one')
+        //             playerTwoArrow[0].classList.toggle('-player-two')
+        //         }
+        //     }, 1000)
+        // }
     }
 
     return /* html */`
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,22 +1,16 @@
 import './style.css'
 
 import PlayerGame from '../../components/PlayerGame'
-import PlayerPoints from '../../components/PlayerPoints'
 import VsPlayer from '../../components/VsPlayer'
-import ArrowPlayer from '../../components/ArrowPlayer'
 import PlayerScore from '../../components/PlayerScore'
 import ArrowDown from '../../components/ArrowDown'
 
-// const $htmlArrowPlayer = ArrowPlayer()
 const $htmlArrowDown = ArrowDown(2)
 const $htmlPlayerGame1 = PlayerGame('Player1')
 const $htmlPlayerGame2 = PlayerGame('Player2')
 const $htmlVsPlayer = VsPlayer('vs')
 const $htmlPlayerPoints1 = PlayerScore(0)
 const $htmlPlayerPoints2 = PlayerScore(3)
-
-// const $htmlPlayerPoints1 = PlayerPoints()
-// const $htmlPlayerPoints2 = PlayerPoints()
 
 const ScoreBoard = () => {
     return /* html */ `      
